597b502b4264006f29fc1b558d224379b1a08789
Noel6161131110 <noelpaulgeorge@gmail.com>
2023-08-19 15:06:43 +0530

    Added bash shell script for execution

---
+++ runapp.sh
@@ -0,0 +1,10 @@
+pip install -r requirements.txt &
+
+# Start Celery Beat
+celery -A your_project_name beat --loglevel=info &
+
+# Start Celery worker
+celery -A your_project_name worker --loglevel=info &
+
+# Start Django development server
+python manage.py runserver
