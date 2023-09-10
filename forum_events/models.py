fd55e87f4d036be96a1b00c58127dbf02d151b8b
Harikrishna A <Harikichus2018@gmail.com>
2023-09-11 01:23:14 +0530

    Added PUT:form events

--- forum_events/models.py
+++ forum_events/models.py
@@ -3,16 +3,29 @@ from django.db import models, connection
 
 
 class forumEvents(models.Model):
-    title = models.TextField()
+
+    title = models.TextField(blank=True)
     content = models.TextField(blank=True,null=True)
-    poster_image = models.ImageField(upload_to='forum/events/posters/')
+    poster_image = models.ImageField(upload_to='forum/events/posters/',blank=True)
+    poster_image_url = models.TextField(blank=True,null=True)
     thumbnail_poster_image = models.ImageField(upload_to='forum/events/thumbnails/', blank=True, null=True) 
+    thumbnail_poster_image_url = models.TextField(blank=True,null=True) 
     register_button_link = models.TextField(default='vcec_form',blank=True,null=True)
     whatsapp_link = models.TextField(blank=True,null=True)
-    status = models.CharField(max_length=10,default='Upcoming')
+    status = models.CharField(max_length=10,default='Upcoming',blank=True)
     publish_date = models.DateTimeField(auto_now_add=True)
-    published_by = models.CharField(max_length=100)
+    published_by = models.CharField(max_length=100,blank=True)
     hashtags = models.TextField(blank=True,null=True)
+    
+    def save(self, *args, **kwargs):
+        if self.poster_image:
+            self.poster_image_url = f"forum/events/cec/api/events/{self.id}/file/"
+        if self.thumbnail_poster_image:
+            self.thumbnail_poster_image_url = f"forum/events/cec/api/events/{self.id}/thumbnail/"
+
+        super().save(*args, **kwargs)
+
+
     
     
 class Registration(models.Model):
@@ -62,7 +75,10 @@ def create_dynamic_models(model_names):
 
 
 def create_tables(app_name,unique_id):
-    model_names = [app_name + '_'+str(unique_id)+'_likes', app_name+'_'+str(unique_id)+'_registration']
+    
+    model_names = [app_name + '_'+str(unique_id)+'_likes' ]
+    if forumEvents.objects.get(pk=unique_id).register_button_link=='vcec_form':
+        model_names.append(app_name+'_'+str(unique_id)+'_registration')
     create_dynamic_models(model_names)
 # def create_like_event_model(event):
 #     class_name = f'LikeEvent{event.id}'
--- forum_events/serializers.py
+++ forum_events/serializers.py
@@ -6,3 +6,7 @@ class FormSerializer(serializers.ModelSerializer):
     class Meta:
         model=forumEvents
         fields=('id','title','content','poster_image','thumbnail_poster_image','register_button_link','whatsapp_link','status','publish_date','published_by','hashtags')
+
+
+
+
