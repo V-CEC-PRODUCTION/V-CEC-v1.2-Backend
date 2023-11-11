7de138c19120dcc90b33765d456ffb192f9a6e5e
Noel6161131110 <noelpaulgeorge@gmail.com>
2023-11-11 20:58:11 +0530

    updated settings.py corsheaders

--- vcec_bk/settings.py
+++ vcec_bk/settings.py
@@ -84,6 +84,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'channels.middleware.WebSocketMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
