08d47a2a8d473831a505d7e396ff5cfbf9fdb7d7
Noel6161131110 <noelpaulgeorge@gmail.com>
2023-11-11 21:58:09 +0530

    updated models audit team score and item list

--- live_update_board/models.py
+++ live_update_board/models.py
@@ -21,15 +21,15 @@ class TeamItems(models.Model):
     
     
 # Define a custom function to be called by triggers
-def update_team_score(sender, instance, **kwargs):
-    if kwargs['created']:
-        operation = 'INSERT'
-    else:
-        operation = 'UPDATE'
+# def update_team_score(sender, instance, **kwargs):
+#     if kwargs['created']:
+#         operation = 'INSERT'
+#     else:
+#         operation = 'UPDATE'
 
-    # Execute a custom SQL statement when a new record is inserted or updated
-    with connection.cursor() as cursor:
-        cursor.execute("INSERT INTO live_update_board_teamscoreaudit (operation, team_id, score) VALUES (%s, %s, %s)", [operation, instance.team, instance.score])
+#     # Execute a custom SQL statement when a new record is inserted or updated
+#     with connection.cursor() as cursor:
+#         cursor.execute("INSERT INTO live_update_board_teamscoreaudit (operation, team_id, score) VALUES (%s, %s, %s)", [operation, instance.team, instance.score])
 
 # Create a database table to store the audit log
 # This table should have fields for operation, team_id, and score, and a timestamp if desired
@@ -40,24 +40,24 @@ class TeamScoreAudit(models.Model):
     score = models.TextField()
 
 # Connect the custom function to the post_save signal
-post_save.connect(update_team_score, sender=TeamScore)
+#post_save.connect(update_team_score, sender=TeamScore)
 
 # Connect the custom function to the post_delete signal
-@receiver(post_delete, sender=TeamScore)
-def delete_team_score(sender, instance, **kwargs):
-    with connection.cursor() as cursor:
-        cursor.execute("INSERT INTO live_update_board_teamscoreaudit (operation, team_id, score) VALUES ('DELETE', %s, %s)", [instance.team, instance.score])
+# @receiver(post_delete, sender=TeamScore)
+# def delete_team_score(sender, instance, **kwargs):
+#     with connection.cursor() as cursor:
+#         cursor.execute("INSERT INTO live_update_board_teamscoreaudit (operation, team_id, score) VALUES ('DELETE', %s, %s)", [instance.team, instance.score])
         
 
-def update_team_item(sender, instance, **kwargs):
-    if kwargs['created']:
-        operation = 'INSERT'
-    else:
-        operation = 'UPDATE'
+# def update_team_item(sender, instance, **kwargs):
+#     if kwargs['created']:
+#         operation = 'INSERT'
+#     else:
+#         operation = 'UPDATE'
 
-    # Execute a custom SQL statement when a new record is inserted or updated
-    with connection.cursor() as cursor:
-        cursor.execute("INSERT INTO live_update_board_teamitemaudit (operation, team_id, score) VALUES (%s, %s, %s)", [operation, instance.team, instance.points])
+#     # Execute a custom SQL statement when a new record is inserted or updated
+#     with connection.cursor() as cursor:
+#         cursor.execute("INSERT INTO live_update_board_teamitemaudit (operation, team_id, score) VALUES (%s, %s, %s)", [operation, instance.team, instance.points])
 
 # Create a database table to store the audit log
 # This table should have fields for operation, team_id, and score, and a timestamp if desired
@@ -68,10 +68,10 @@ class TeamItemAudit(models.Model):
     score = models.TextField()
 
 # Connect the custom function to the post_save signal
-post_save.connect(update_team_item, sender=TeamItems)
+# post_save.connect(update_team_item, sender=TeamItems)
 
-# Connect the custom function to the post_delete signal
-@receiver(post_delete, sender=TeamItems)
-def delete_team_score(sender, instance, **kwargs):
-    with connection.cursor() as cursor:
-        cursor.execute("INSERT INTO live_update_board_teamitemaudit (operation, team_id, score) VALUES ('DELETE', %s, %s)", [instance.team, instance.points])
+# # Connect the custom function to the post_delete signal
+# @receiver(post_delete, sender=TeamItems)
+# def delete_team_score(sender, instance, **kwargs):
+#     with connection.cursor() as cursor:
+#         cursor.execute("INSERT INTO live_update_board_teamitemaudit (operation, team_id, score) VALUES ('DELETE', %s, %s)", [instance.team, instance.points])
--- vcec_bk/settings.py
+++ vcec_bk/settings.py
@@ -83,7 +83,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'channels.middleware.WebSocketMiddleware',
+    #'channels.middleware.WebSocketMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
