80e387183de398a091fe5ead3396df35ee675944
Noel6161131110 <noelpaulgeorge@gmail.com>
2024-01-19 15:52:48 +0530

    changes in settings.py

--- app/vcec_bk/urls.py
+++ app/vcec_bk/urls.py
@@ -7,15 +7,15 @@ urlpatterns = [
     path('users/auth/', include('users.urls')),
     path('notices/nav/', include('notices.urls')),
     path('homepage/', include('homepage_images.urls')),
-    path('staff/info/',include('staff_info.urls')),
-    path('highlights/cec/',include('highlights_cec.urls')),
-    path('gallery/cec/',include('gallery_cec.urls')),
-    path('forum/events/',include('forum_events.urls')),
-    path('forum/announcements/',include('forum_announcements.urls')),
-    path('timetable/cec/',include('timetables.urls')),
+    path('staff/info/', include('staff_info.urls')),
+    path('highlights/cec/', include('highlights_cec.urls')),
+    path('gallery/cec/', include('gallery_cec.urls')),
+    path('forum/events/', include('forum_events.urls')),
+    path('forum/announcements/', include('forum_announcements.urls')),
+    path('timetable/cec/', include('timetables.urls')),
     path('forum/management/', include('forum_management.urls')),
     path('forum/stories/', include('forum_stories.urls')),
     path('fixtures/ashwa/', include('fixtures_ashwa.urls')),
 ]
 
-urlpatterns += staticfiles_urlpatterns()    
+urlpatterns += staticfiles_urlpatterns()
