fdfdc4d56dc781ca8018ca1f9b056a1f4e86724d
Noel Paul George <noelpaulgeorge@Noels-MacBook-Pro.local>
2024-03-05 13:32:33 +0530

    formatted py files

--- app/vcec_bk/azure_storages.py
+++ app/vcec_bk/azure_storages.py
@@ -1,6 +1,7 @@
 from storages.backends.azure_storage import AzureStorage
 import os
 
+
 class AzureMediaStorage(AzureStorage):
     account_name = os.getenv('AZURE_STORAGE_ACCOUNT_NAME')
     account_key = os.getenv('AZURE_ACCOUNT_KEY')
