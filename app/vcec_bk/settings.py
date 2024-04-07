66d3d3fd4e849394419d5e6c23caddc1b851a152
Noel Paul George <noelpaulgeorge@Noels-MacBook-Pro.local>
2024-04-07 20:46:38 +0530

    flake error docker issue resolved

--- app/vcec_bk/settings.py
+++ app/vcec_bk/settings.py
@@ -159,11 +159,10 @@ DATABASES = {
 }
 
 
-
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 1,
-    
+
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
